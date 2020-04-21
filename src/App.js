f8945b03a0e8d788ef95ba7844062fd8a120e905
Mark Simpkins <msimpkins@gmail.com>
2020-04-21 20:07:40 +0100

    initial commit - Just been trying to get some basic steps working with Amplify

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
+import Amplify, { Analytics, Storage } from 'aws-amplify';
+import awsconfig from './aws-exports';
+import { withAuthenticator, S3Album } from 'aws-amplify-react'; 
 import './App.css';
+import '@aws-amplify/ui/dist/style.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+Amplify.configure(awsconfig);
+Storage.configure({ level: 'private' });
+
+class App extends React.Component {
+  uploadFile = (evt) => {
+    const file = evt.target.files[0];
+    const name = file.name;
+
+    Storage.put(name, file).then(() => {
+      this.setState({ file: name });
+    })
+  }
+
+  componentDidMount() {
+    Analytics.record('Amplify_CLI');
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <p> Pick a file</p>
+        <input type="file" onChange={this.uploadFile} />
+        <S3Album level="private" path='' />
+      </div>
+    );
+  }
 }
 
-export default App;
+export default withAuthenticator(App, true);
